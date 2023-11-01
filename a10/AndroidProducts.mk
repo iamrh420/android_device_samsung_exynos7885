ef3966fff2c58e030f12bfdab8c1f5ef24583636
iamrh420 <92685435+iamrh420@users.noreply.github.com>
2023-11-01 22:26:59 +0600

    Create AndroidProducts.mk

---
+++ a10/AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/a10_lavender.mk
+
+COMMON_LUNCH_CHOICES := \
+    blaze_a10-user \
+    blaze_a10-userdebug \
+    blaze_a10-eng
