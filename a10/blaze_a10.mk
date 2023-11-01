0c4bdfb087bdbe024140af3ee4b23ed9473790cf
iamrh420 <92685435+iamrh420@users.noreply.github.com>
2023-11-01 22:38:37 +0600

    Create blaze_a10.mk

---
+++ a10/blaze_a10.mk
@@ -0,0 +1,33 @@
+# Copyright (C) 2018 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/a10/device.mk)
+
+# Inherit some common ProjectBlaze stuff
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+BLAZE_BUILD_TYPE := UNOFFICIAL
+BLAZE_MAINTAINER := Ʀᴀ㉿ɪƁ
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 720
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a10
+PRODUCT_NAME := blaze_a10
+PRODUCT_MODEL := SM-A105F
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-google
+
+BUILD_FINGERPRINT := "google/cheetah/cheetah:13/TQ3A.230705.001/10216780:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRIVATE_BUILD_DESC="cheetah-user 13 TQ3A.230705.001 10216780 release-keys"
